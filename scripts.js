ee494714fd17618ebc986964284a360fcb88a470
Nikolas Kouloufakos <nikolas.kouloufakos@shopify.com>
2018-01-05 13:22:23 -0500

    update button appearance and documenting js function

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
+/* changeVideo demonstrates how you can change an
+DOM attribute using javacript functions*/
 function changeVideo() {
 	let video1 = "https://www.youtube.com/embed/E4Eoev6a6nQ";
 	let video2 = "https://www.youtube.com/embed/i_wT_Tt6Jc4";
 	let video3 = "https://www.youtube.com/embed/g9wcM5qztXY";
-	
+
 	if (document.getElementById('youtube').src == video1) {
 		document.getElementById('youtube').src = video2;
 	} else if (document.getElementById('youtube').src == video2) {
