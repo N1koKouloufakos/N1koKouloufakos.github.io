350b85b3d1114188da12e40fee397fcfe0dd97cb
Nikolas Kouloufakos <nikolas.kouloufakos@shopify.com>
2018-01-05 13:06:28 -0500

    add main page and resume page

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
-function openGithub() {
-	document.getElementById()
+function changeVideo() {
+	let video1 = "https://www.youtube.com/embed/E4Eoev6a6nQ";
+	let video2 = "https://www.youtube.com/embed/i_wT_Tt6Jc4";
+	if (document.getElementById('youtube').src == video1) {
+		document.getElementById('youtube').src = video2;
+	} else {
+		document.getElementById('youtube').src = video1;
+	}
+}
+
+function openResume() {
+	window.location.href = "resume.html";
 }
