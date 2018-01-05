98b6c347e622ec179a3f1d5821b1ddafc0bb1b71
Nikolas Kouloufakos <nikolas.kouloufakos@shopify.com>
2018-01-05 13:13:50 -0500

    added new iframe video

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
 function changeVideo() {
 	let video1 = "https://www.youtube.com/embed/E4Eoev6a6nQ";
 	let video2 = "https://www.youtube.com/embed/i_wT_Tt6Jc4";
+	let video3 = "https://www.youtube.com/embed/g9wcM5qztXY";
+	
 	if (document.getElementById('youtube').src == video1) {
 		document.getElementById('youtube').src = video2;
+	} else if (document.getElementById('youtube').src == video2) {
+		document.getElementById('youtube').src = video3;
 	} else {
 		document.getElementById('youtube').src = video1;
 	}
